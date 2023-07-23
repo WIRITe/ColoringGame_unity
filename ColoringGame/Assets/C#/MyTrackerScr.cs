4dfe988980fbbd985150a5eaa3c6371cdc16994b
eWIRITe <118571958+eWIRITe@users.noreply.github.com>
2023-07-22 21:13:35 -0400

    import MyTracker

---
+++ ColoringGame/Assets/C#/MyTrackerScr.cs
@@ -0,0 +1,28 @@
+﻿using Mycom.Tracker.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTrackerScr : MonoBehaviour
+{
+    public string android_SDK_key;
+    public string IOs_SDK_key;
+
+    public void Awake()
+    {
+#if !UNITY_IOS && !UNITY_ANDROID
+        return;
+#endif
+
+        // При необходимости настройте конфигурацию трекера
+        var myTrackerConfig = MyTracker.MyTrackerConfig;
+        var myTrackerParams = MyTracker.MyTrackerParams;
+
+        // Инициализируйте трекер в зависимости от платформы
+#if UNITY_IOS
+        MyTracker.Init(IOs_SDK_key);
+#elif UNITY_ANDROID
+        MyTracker.Init(android_SDK_key);
+#endif
+    }
+}
